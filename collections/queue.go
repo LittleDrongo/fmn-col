e181d8ea7f0df4f77abf4266dd0a4bded9781343
Fomin Alexey <fomin.alexey@outlook.com>
2024-08-24 19:58:20 +0200

    delete any queue

--- collections/queue.go
+++ collections/queue.go
@@ -2,34 +2,29 @@ package collections
 
 import "sync"
 
-type Queue struct {
-	items []any
+type queue[T any] struct {
+	items []T
 	mutex sync.Mutex
 }
 
-func NewQueue() *Queue {
-	return &Queue{items: make([]any, 0)}
+func NewQueue[T any]() *queue[T] {
+	return &queue[T]{items: make([]T, 0)}
 }
 
-func (q *Queue) Put(item any) {
+func (q *queue[T]) Put(item T) {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
 	q.items = append(q.items, item)
 }
 
-func (q *Queue) Get() any {
+func (q *queue[T]) Get() *T {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
-
 	if len(q.items) == 0 {
 		return nil
 	}
 
 	item := q.items[0]
 	q.items = q.items[1:]
-	return item
-}
-
-func (q *Queue) Size() int {
-	return len(q.items)
+	return &item
 }
--- sample.go
+++ sample.go
@@ -5,34 +5,9 @@ import (
 	"queue/collections"
 )
 
-func main() {
+func sampleQueueString() {
 
-	queue := collections.NewQueue()
+	stringQueue := collections.NewQueue[string]()
+	fmt.Println(stringQueue.Get())
 
-	fmt.Println("queue size is: ", queue.Size())
-
-	queue.Put(1)
-
-	queue.Put(2)
-
-	queue.Put("world")
-
-	queue.Put('x')
-
-	fmt.Println("queue size is: ", queue.Size())
-
-	queue.Put(func() {
-		fmt.Printf("func test")
-
-	})
-
-	for item := queue.Get(); item != nil; item = queue.Get() {
-
-		switch v := item.(type) {
-		case func():
-			v()
-		default:
-			fmt.Println(v)
-		}
-	}
 }
